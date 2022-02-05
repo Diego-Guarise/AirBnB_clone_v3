ae1834d70fcfdea3224a0c506ae34b54df900306
Diego-Guarise <l.guariselopez@gmail.com>
2022-02-05 12:44:28 -0300

    fix imports

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -6,6 +6,7 @@ from api.v1.views import app_views
 from models import storage
 from models.amenity import Amenity
 from models.place import Place
+from os import environ
 STORAGE_TYPE = environ.get('HBNB_TYPE_STORAGE')
 
 
