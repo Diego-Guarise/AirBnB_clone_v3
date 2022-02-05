e5fe5bd5e28448b5327136a8d1c3b13289ae4eec
Diego-Guarise <l.guariselopez@gmail.com>
2022-02-05 12:23:42 -0300

    add advanced

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""All Place CRUD operations"""
+
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.place import Place
+
+
+@app_views.route('/places/<place_id>/amenities', methods=['GET'])
+def get_amenitites(place_id):
+    """Create a new view for the link between Place objects and Amenity"""
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    if request.method == 'GET':
+        amenities = storage.all(Amenity).values()
+        place_amenities = []
+        for amenity in amenities:
+            if amenity.place_id == place_id:
+                place_amenities.append(amenity.to_dict())
+        return jsonify(place_amenities)
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE', 'POST'])
+def get_amenity(place_id, amenity_id):
+    """get amenity by place id and amenity id"""
+    place = storage.get(Place, place_id)
+    if not place:
+        abort(404)
+    amenity = storage.get(Amenity, amenity_id)
+    if not amenity:
+        abort(404)
+    if request.method == 'DELETE':
+        if place.id != amenity.place_id:
+            abort(404)
+        storage.delete(amenity)
+        storage.save()
+        return jsonify({}), 200
+    else:
+        if place.id == amenity.place_id:
+            return jsonify(amenity.to_dict()), 200
+        return Amenity(), 201
