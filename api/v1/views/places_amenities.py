348c1141c585a580acb4d341f1e091a3cc6b658d
Diego-Guarise <l.guariselopez@gmail.com>
2022-02-05 12:54:46 -0300

    fix "depending of the storage"

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -14,14 +14,17 @@ STORAGE_TYPE = environ.get('HBNB_TYPE_STORAGE')
 def get_amenitites(place_id):
     """Create a new view for the link between Place objects and Amenity"""
     place = storage.get('Place', place_id)
-    if not place:
+    if place is None:
         abort(404)
     if request.method == 'GET':
         amenities = storage.all('Amenity').values()
-        place_amenities = []
-        for amenity in amenities:
-            if amenity.place_id == place_id:
-                place_amenities.append(amenity.to_dict())
+        if STORAGE_TYPE == 'db':
+            place_amenities = place.amenities
+        else:
+            place_amenities = []
+            for amenity in amenities:
+                if amenity.place_id == place_id:
+                    place_amenities.append(amenity.to_dict())
         return jsonify(place_amenities)
 
 
@@ -30,10 +33,10 @@ def get_amenitites(place_id):
 def get_amenity(place_id, amenity_id):
     """get amenity by place id and amenity id"""
     place = storage.get('Place', place_id)
-    if not place:
+    if place is None:
         abort(404)
     amenity = storage.get('Amenity', amenity_id)
-    if not amenity:
+    if amenity is None:
         abort(404)
     if request.method == 'DELETE':
         if place.id != amenity.place_id:
